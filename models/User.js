543938bf0701bd3e238b084bd2fa536805a7d796
YeonukLim <lim454@naver.com>
2017-02-26 18:43:49 +0900

    update 비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // models/User.js
 
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs"); // 1
 
 // schema // 1
 var userSchema = mongoose.Schema({
@@ -11,7 +12,6 @@ var userSchema = mongoose.Schema({
 },{
  toObject:{virtuals:true}
 });
-
 
 // virtuals // 2
 userSchema.virtual("passwordConfirmation")
@@ -43,13 +43,13 @@ userSchema.path("password").validate(function(v) {
    user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
   }
  }
- 
+
  // update user // 3-4
  if(!user.isNew){
   if(!user.currentPassword){
    user.invalidate("currentPassword", "Current Password is required!");
   }
-  if(user.currentPassword && user.currentPassword != user.originalPassword){
+  if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)){ // 2
    user.invalidate("currentPassword", "Current Password is invalid!");
   }
   if(user.newPassword !== user.passwordConfirmation) {
@@ -57,6 +57,23 @@ userSchema.path("password").validate(function(v) {
   }
  }
 });
+
+// hash password // 3
+userSchema.pre("save", function (next){
+ var user = this;
+ if(!user.isModified("password")){ // 3-1
+  return next();
+ } else {
+  user.password = bcrypt.hashSync(user.password); // 3-2
+  return next();
+ }
+});
+
+// model methods // 4
+userSchema.methods.authenticate = function (password) {
+ var user = this;
+ return bcrypt.compareSync(password,user.password);
+};
 
 // model & export
 var User = mongoose.model("user",userSchema);
